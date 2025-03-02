829d8be6ace6907e7407b5ce32d776f7942df9d3
MalithMadusankha <malithsony@gmail.com>
2025-03-02 12:19:53 +0530

    add again

--- main.py
+++ main.py
@@ -33,3 +33,7 @@ async def read_root():
     return {"message": "Hello !!!, I am FastAPI Server. U can call my API I am here to responed "}
 
 print("<============== Server started ==============>")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
